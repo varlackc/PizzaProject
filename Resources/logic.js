f5dbec620f1a42a7935c506d3cfd8c38456eb889
varlackc <varlackc@mail.gvsu.edu>
2019-10-09 23:16:20 -0500

    Create Object Array
    Create Object Array and display data in console log

--- Resources/logic.js
+++ Resources/logic.js
@@ -1,6 +1,23 @@
-$.getJSON('http://files.olo.com/pizzas.json', function(data) {
-    //data is the JSON string
+$.getJSON('http://files.olo.com/pizzas.json', function(data) { // data is the Json String
+
+    //declare a toppings array
+    var toppingsArray = new Array();
     data.forEach(element => {
-        console.log(element);
+        //write in console
+        //console.log(element.toppings);
+
+        // if the topping is not in the list then add
+
+        if (!(toppingsArray.includes(element.toppings))) {
+            toppingsArray.push({ "Topping": element.toppings, "Count": 1 });
+            //console.log("Added " + element.toppings);
+        }
+
+        //write on browser
+        //document.writeln(element.toppings + " ");
+    });
+
+    toppingsArray.forEach(element => {
+        console.log("Topping: " + element.Topping + " Count: " + element.Count);
     });
 });
