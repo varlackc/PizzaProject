71a47919f315d46d13d74319fbdf87ac880aeeff
varlackc <varlackc@mail.gvsu.edu>
2019-10-10 20:31:23 -0500

    Add function to show content
    Add function to show content

---
+++ Resources/logic.js
@@ -0,0 +1,6 @@
+function showContent() {
+    var content = "<h2>This Application Shows The Most Popular Toppings</h2>";
+    content += '<table class="table"><thead><tr><th scope="col">#</th scope ="col"><th scope="col">Topping</th><th scope="col">Count</th></tr></thead>';
+    content += "<tbody>";
+    document.write(content);
+}
